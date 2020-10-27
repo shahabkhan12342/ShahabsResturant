fe2fe0a2838fb04fbea0d85bd250e6278ab561b5
shahabkhan12342 <shahabkhan12342@gmail.com>
2020-10-28 02:55:41 +0500

    ShahabResturant

---
+++ app.js
@@ -0,0 +1,101 @@
+function addToCart()
+{
+    var img1=document.getElementById('img1');
+    var name1=document.getElementById('name1')
+    var price1=document.getElementById('price1')
+
+    var cartimg1=document.getElementById('cart-img1');
+    var cartname1=document.getElementById('cart-name1')
+    var cartprice1=document.getElementById('cart-price1')
+    
+    cartimg1.src=img1.src;
+    cartname1.innerHTML=name1.innerHTML;
+    cartprice1.innerHTML=price1.innerHTML;
+}
+function addToCart2()
+{
+    var img2=document.getElementById('img2');
+    var name2=document.getElementById('name2')
+    var price2=document.getElementById('price2')
+
+    var cartimg2=document.getElementById('cart-img2');
+    var cartname2=document.getElementById('cart-name2')
+    var cartprice2=document.getElementById('cart-price2')
+    
+    cartimg2.src=img2.src;
+    cartname2.innerHTML=name2.innerHTML;
+    cartprice2.innerHTML=price2.innerHTML;
+}
+
+function addToCart3()
+{
+    var img3=document.getElementById('img3');
+    var name3=document.getElementById('name3')
+    var price3=document.getElementById('price3')
+
+    var cartimg3=document.getElementById('cart-img3');
+    var cartname3=document.getElementById('cart-name3')
+    var cartprice3=document.getElementById('cart-price3')
+    
+    cartimg3.src=img3.src;
+    cartname3.innerHTML=name3.innerHTML;
+    cartprice3.innerHTML=price3.innerHTML;
+}
+
+function addToCart4()
+{
+    var img4=document.getElementById('img4');
+    var name4=document.getElementById('name4')
+    var price4=document.getElementById('price4')
+
+    var cartimg4=document.getElementById('cart-img4');
+    var cartname4=document.getElementById('cart-name4')
+    var cartprice4=document.getElementById('cart-price4')
+    
+    cartimg4.src=img4.src;
+    cartname4.innerHTML=name4.innerHTML;
+    cartprice4.innerHTML=price4.innerHTML;
+}
+
+function addToCart5()
+{
+    var img5=document.getElementById('img5');
+    var name5=document.getElementById('name5')
+    var price5=document.getElementById('price5')
+
+    var cartimg5=document.getElementById('cart-img5');
+    var cartname5=document.getElementById('cart-name5')
+    var cartprice5=document.getElementById('cart-price5')
+    
+    cartimg5.src=img5.src;
+    cartname5.innerHTML=name5.innerHTML;
+    cartprice5.innerHTML=price5.innerHTML;
+}
+function addToCart6()
+{
+    var img6=document.getElementById('img6');
+    var name6=document.getElementById('name6')
+    var price6=document.getElementById('price6')
+
+    var cartimg6=document.getElementById('cart-img6');
+    var cartname6=document.getElementById('cart-name6')
+    var cartprice6=document.getElementById('cart-price6')
+    
+    cartimg6.src=img6.src;
+    cartname6.innerHTML=name6.innerHTML;
+    cartprice6.innerHTML=price6.innerHTML;
+}
+function addToCart7()
+{
+    var img7=document.getElementById('img7');
+    var name7=document.getElementById('name7')
+    var price7=document.getElementById('price7')
+
+    var cartimg7=document.getElementById('cart-img7');
+    var cartname7=document.getElementById('cart-name7')
+    var cartprice7=document.getElementById('cart-price7')
+    
+    cartimg7.src=img7.src;
+    cartname7.innerHTML=name7.innerHTML;
+    cartprice7.innerHTML=price7.innerHTML;
+}
